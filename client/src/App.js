5feb14bb7aa0e50d5b760383e2e478c9f1a0008c
dragonbegin <juan.reyes.mba@gmail.com>
2019-06-20 12:15:23 -0500

    added Route path to MovieList and Movie:id in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import {Route} from 'react-router-dom';
 
 export default class App extends Component {
   constructor() {
@@ -23,6 +24,8 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <div>Replace this Div with your Routes</div>
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/Movie/:id" component={Movie} />
       </div>
     );
   }
